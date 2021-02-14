9c51d7478117829230d720628a50154126d4a3b4
BrandenSoropia <b.soropia@gmail.com>
2021-02-14 11:54:23 -0500

    Style update.

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -8,16 +8,24 @@
     only, thinking it would be more intereactive swiping/using touch controls.
   </p>
   <strong>Try it out:</strong>
-  <nav>
-    <a class="nav-link" href="/locks" use:link
-      >{"\u{1F512} See and test locks here"}</a
-    >
-    <p>
-      {"\u{1F3D7} (In-Dev) Simple Start to Finish Escape Room"}
-    </p>
-    <p>
-      {"\u{2728} (End Goal) Escape Room w/ Audio and Visuals!"}
-    </p>
+  <nav id="nav-internal">
+    <ul>
+      <li>
+        <a class="nav-link" href="/locks" use:link
+          >{"\u{1F512} See and test locks here"}</a
+        >
+      </li>
+      <li>
+        <p>
+          {"\u{1F3D7} (In-Dev) Simple Start to Finish Escape Room"}
+        </p>
+      </li>
+      <li>
+        <p>
+          {"\u{2728} (End Goal) Escape Room w/ Audio and Visuals!"}
+        </p>
+      </li>
+    </ul>
   </nav>
   <p>When you're done, please let me know what you think here:</p>
   <ul>
@@ -36,4 +44,18 @@
 </section>
 
 <style>
+  strong {
+    text-decoration: underline;
+  }
+
+  nav#nav-internal {
+    margin-top: 0.8rem;
+  }
+
+  nav#nav-internal > ul {
+    padding: 0.8rem;
+  }
+  nav#nav-internal > ul > li {
+    list-style-type: none;
+  }
 </style>
